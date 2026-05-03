d4d744a16dfdc80f9834dbea9383240edea1ac5f
Man4hard <107409042+Man4hard@users.noreply.github.com>
2026-05-03 13:31:47 +0500

    Add Assets/_Core/Scripts/State/GameState.cs

---
+++ Assets/_Core/Scripts/State/GameState.cs
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 [YOUR NAME HERE]. MIT License — see LICENSE.txt
+
+namespace ArrowsPuzzle.Core
+{
+    public enum GameState
+    {
+        None,
+        Loading,
+        Playing,
+        Won,
+        Lost,
+        Paused
+    }
+}
